b9329954c5ffe2ef657b84ea6432347d16c35ab7
dafengzhen <dafengzhen@aliyun.com>
2021-05-21 17:01:55 +0800

    feat: 实现我的预约列表

--- src/main/java/com/example/reserveparkingspace/controller/ParkingRestController.java
+++ src/main/java/com/example/reserveparkingspace/controller/ParkingRestController.java
@@ -1,10 +1,14 @@
 package com.example.reserveparkingspace.controller;
 
+import com.example.reserveparkingspace.entity.CarEntity;
+import com.example.reserveparkingspace.entity.UserEntity;
 import com.example.reserveparkingspace.repository.ParkingReservationRepo;
 import com.example.reserveparkingspace.service.ParkingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +18,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 /**
  * parking restController
@@ -72,6 +77,11 @@ public class ParkingRestController {
         parkingService.addParkingReservation(num, parkingRequest);
 
         return ResponseEntity.ok().body("预约停车位成功");
+    }
+
+    @GetMapping("myParkingList")
+    public ResponseEntity<Set<CarEntity>> myParkingList(Authentication authentication) {
+        return ResponseEntity.ok().body(((UserEntity) authentication.getPrincipal()).getCarList());
     }
 
 }
